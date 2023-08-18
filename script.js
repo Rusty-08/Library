29546bf09505562febac7f36e5db0189e80d59fa
unknown <gunaorusty@gmail.com>
2023-08-18 22:23:12 +0800

    build add-book structure

--- script.js
+++ script.js
@@ -1,13 +1,63 @@
-function Book(title, author, pages, read) {
+// TODO: create a create a object constructor to get the added book
+// TODO: create a card to display in the container
+
+const bookContainer = document.getElementById('books-container');
+
+function Book() {
     this.title = title,
         this.author = author,
-        this.pages = pages,
-        this.read = read,
-        this.info = function () {
-            return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
+        this.page = page,
+        this.read = function () {
+            return
         }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkein', '295 pages', 'not read yet')
+const createBookCard = () => {
 
-console.log(theHobbit.info())
+    let cardHeader = createCardHeader()
+    let cardBody = createCardBody()
+
+    let card = document.createElement('div');
+    card.className = 'card'
+
+    card.appendChild(cardHeader)
+    card.appendChild(cardBody)
+}
+
+const createCardHeader = () => {
+    let bookNumber = document.createElement('p');
+    let bookPage = document.createElement('p');
+    let cardHeader = document.createElement('div');
+
+    bookNumber.setAttribute('id', 'book-number')
+    bookPage.setAttribute('id', 'page')
+
+    cardHeader.className = 'card-header'
+
+    cardHeader.appendChild(bookPage)
+    cardHeader.appendChild(bookNumber)
+}
+
+const createCardBody = () => {
+    let cardTitle = document.createElement('h5');
+    let cardAuthor = document.createElement('p');
+    let cardRead = document.createElement('a');
+    let removeCard = document.createElement('a');
+    let cardBody = document.createElement('div');
+
+    cardTitle.setAttribute('id', 'title')
+    cardAuthor.setAttribute('id', 'author')
+    cardRead.setAttribute('id', 'read-btn')
+    removeCard.setAttribute('id', 'remove-btn')
+
+    cardTitle.className = 'card-title'
+    cardAuthor.className = 'card-subtitle fw-light mb-3'
+    cardRead.className = 'btn btn-success btn-sm'
+    removeCard.className = 'btn btn-danger btn-sm'
+    cardBody.className = 'card-body text-center'
+
+    cardBody.appendChild(cardTitle)
+    cardBody.appendChild(cardAuthor)
+    cardBody.appendChild(cardRead)
+    cardBody.appendChild(removeCard)
+}
